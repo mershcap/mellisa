81f1a9a2646a287446b8f29dcd588eaf8b459d88
mershcap <85987187+mershcap@users.noreply.github.com>
2022-07-14 18:29:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app=Flask(__name__)
 
 #function to load the model
 def Load():
-	return pickle.load(open('player_rating.pkl','rb'))
+	return pickle.load(open('maize_model.pkl','rb'))
 
 #loading defalut page
 @app.route('/')
@@ -17,22 +17,24 @@ def index():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-	gk1=float(request.form.get("gk1",""))
-	gk2=float(request.form.get("gk2",""))
-	gk3=float(request.form.get("gk3",""))
-	gk4=float(request.form.get("gk4",""))
-	gk5=float(request.form.get("gk5",""))
-	gk6=float(request.form.get("gk6",""))
-	gk7=float(request.form.get("gk7",""))
-	gk8=float(request.form.get("gk8",""))
-	gk9=float(request.form.get("gk9",""))
-	gk10=float(request.form.get("gk10",""))
-	features=[gk1,gk2,gk3,gk4,gk5,gk6,gk7,gk8,gk9,gk10]
-	values=[np.array(features)]
-	print(values)
-	model=Load()
-	y_pred=model.predict(values)
-	return render_template('index.html',output='The player predicted score is:{}'.format(y_pred))
+	n_model=Load()
+	img=request.files['file']
+	img = image.load_img(img, target_size=(200, 200))
+	img_array = image.img_to_array(img)
+	img_batch = np.expand_dims(img_array, axis=0)
+	img_p= preprocess_input(img_batch)
+	pred = n_model.predict(img_p)
+	n=pred[0]
+	prediction=''
+	if(n[0]==1.0):
+		prediction='blight'
+	if(n[1]==1.0):
+		prediction='rust'
+	if(n[2]==1.0):
+		prediction='gray'
+	if(n[3]==1.0):
+		prediction='healthy'
+	return render_template('index.html',output='The plant is/has :{}'.format(prediction))
 
 if __name__=='__main__':
 	port=int(os.environ.get('PORT',5000))
